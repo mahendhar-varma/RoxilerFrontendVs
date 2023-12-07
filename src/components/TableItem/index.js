7191b817434a34d7591b7c9a2079c9ac3b5b3b63
Mahendhar <7093741662mahiv@gmail.com>
2023-12-07 11:14:14 +0530

    some changes done

--- src/components/TableItem/index.js
+++ src/components/TableItem/index.js
@@ -2,21 +2,22 @@ import "./index.css";
 
 const TableItem = (props) => {
   const { data } = props;
-  const { id, title, description, price, category } = data;
-  const image = "https://i.ibb.co/C5Cs4Pd/marguerite-729510-1280.jpg";
+  const { id, title, description, category, image } = data;
+  let {price} = data 
+  const updatedPrice = price.toFixed(2)
 
   let { sold } = data;
   if (sold === 1) {
-    sold = "True";
+    sold = "Sold";
   } else {
-    sold = "False";
+    sold = "Not sold";
   }
   return (
     <>
       <li className="tableText">{id}</li>
       <li className="titleText tableText">{title}</li>
       <li className="texts tableText">{description}</li>
-      <li className="tableText">{price}</li>
+      <li className="tableText">{updatedPrice}</li>
       <li className="tableText">{category}</li>
       <li className="tableText">{sold}</li>
       <li className="tableText forBorderNone">
